5720343bba02b957e4717af23f824654c31b719d
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:45:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "manheim-autoconnect-45906",
+  url: "https://manheim-autoconnect-45906.botics.co",
+  api: "https://manheim-autoconnect-45906.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
